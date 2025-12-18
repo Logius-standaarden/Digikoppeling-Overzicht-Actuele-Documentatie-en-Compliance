91967aaf5b68fe176fc5a5c5a55147921ca7021b
Alexander Green <alexander.green@logius.nl>
2025-12-18 14:14:04 +0100

    Patch 1.21.2 (#16)
    * Beheer: remove alternateFormats from configuration
    
    * Voetnootsplitsing en versienummer
    
    * Data geldigheid voorgaande versies
    
    * Gedeelde voetnoot onderaan hoofdstuk

--- js/config.mjs
+++ js/config.mjs
@@ -1,10 +1,6 @@
 import { loadRespecWithConfiguration } from "https://logius-standaarden.github.io/publicatie/respec/organisation-config.mjs";
 
 loadRespecWithConfiguration({
-  alternateFormats: [ { 
-        "label" : "pdf",
-        "uri" : "Digikoppeling-Overzicht-Actuele-Documentatie-en-Compliance.pdf"
-      } ],
   authors: [ { 
         "name" : "Pieter Hering",
         "url" : "https://logius.nl/standaarden"
@@ -22,11 +18,11 @@ loadRespecWithConfiguration({
       }
     ],
   github: "https://github.com/Logius-standaarden/Digikoppeling-Overzicht-Actuele-Documentatie-en-Compliance",
-  previousPublishDate: "2025-01-30",
-  previousPublishVersion: "1.11.0",
+  previousPublishDate: "2025-05-15",
+  previousPublishVersion: "1.12.1",
   pubDomain: "dk",
-  publishDate: "2025-05-15",
-  publishVersion: "1.12.1",
+  publishDate: "2025-11-05",
+  publishVersion: "1.12.2",
   shortName: "actueel",
   specStatus: "DEF",
   specType: "ST"
